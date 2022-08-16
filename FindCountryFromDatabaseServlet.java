9e62eb7cf6eb578bb7014c6687def45ed413a2e8
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-08-16 17:13:57 +0530

    Add files via upload

---
+++ FindCountryFromDatabaseServlet.java
@@ -0,0 +1,128 @@
+package com.ds;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.TreeMap;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * Servlet implementation class FindCountryServlet
+ */
+@WebServlet("/findCountryFromDB")
+public class FindCountryFromDatabaseServlet extends GenericServlet {
+	/*
+	INSERT INTO MYCountries values ("India", "New Delhi", "Mr. Narendra Modi", "136.64 Crores", "Rs");
+	INSERT INTO MYCountries values ("Pakistan", "Islamabad", "Mr. Imran Khan", "21.66 Crores", "Pakistani Rupee");
+	INSERT INTO MYCountries values ("China", "Beijing", "Mr. Xi Jinping", "139.77 Crores", "Yuan");
+	INSERT INTO MYCountries values ("England", "London", "Mr. Boriss Johnson", "6.66 Crores", "Pound Sterling");
+	INSERT INTO MYCountries values ("America", "Washington DC", "Mr. Joe Biden", "32.82 Crores", "USD");
+	*/
+	
+	Connection conn; //global ref
+
+    public FindCountryFromDatabaseServlet() {
+        super();
+        System.out.println("FindCountryFromDatabaseServlet()");   	
+        try {
+			Class.forName("org.hsqldb.jdbc.JDBCDriver");
+			System.out.println("Database Driver loaded....");
+			conn = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/mydb", "SA", "");
+		    System.out.println("Connected to the DB : "+conn);	
+			  
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+      
+    }
+
+	
+	public void init(ServletConfig config) throws ServletException {
+	     System.out.println("init");
+	}
+
+
+	public void destroy() {
+	     System.out.println("destroy");
+	}
+
+	
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+	    System.out.println("\t\tservice()");
+	    response.setContentType("text/html");
+	    String country =  request.getParameter("countryName"); //read this HTML tag's value
+	    PrintWriter pw = response.getWriter();
+	    try {
+			Statement st = conn.createStatement();
+			System.out.println("statement created.."+st);
+			ResultSet rs = st.executeQuery("SELECT * FROM MYCOUNTRIES");
+			System.out.println("Query fired...got the result...");
+			pw.println("<TABLE border=5 cellspacing=10 cellpadding=10>");
+			
+			pw.println("<TH>Country</TH>");
+			pw.println("<TH>Capital</TH>");
+			pw.println("<TH>Prime Minister</TH>");
+			pw.println("<TH>Population</TH>");
+			pw.println("<TH>Currency</TH>");
+			
+			pw.println("<TR>");
+			pw.println("<TD><input type=text name='countryName'></TD>");
+			pw.println("<TD><input type=text name='capitalName'></TD>");
+			pw.println("<TD><input type=text name='pmName'></TD>");
+			pw.println("<TD><input type=text name='population'></TD>");
+			pw.println("<TD><input type=text name='currency'></TD>");
+			pw.println("<TD><button style='font-size:16px; text-align:center; padding: 15px 32px; color:white; background-color:blue'>Add</button></TD>");
+			pw.println("</TR>");
+			
+			
+
+			while(rs.next()) {
+				pw.println("<TR>");
+				
+				String foundCountryName = rs.getString(1);
+				String foundCapitalName = rs.getString(2);
+				String foundPrimeMinister = rs.getString(3);
+				String foundPopulation = rs.getString(4);
+				String foundCurrency = rs.getString(5);
+				
+				
+				pw.println("<TD>"+foundCountryName+"</TD>");
+				pw.println("<TD>"+foundCapitalName+"</TD>");
+				pw.println("<TD>"+foundPrimeMinister+"</TD>");
+				pw.println("<TD>"+foundPopulation+"</TD>");
+				pw.println("<TD>"+foundCurrency+"</TD>");
+				
+				pw.println("<TD><button style='font-size:16px; text-align:center; padding: 15px 32px; color:white; background-color:green'>Edit</button></TD>");
+				pw.println("<TD><button style='font-size:16px; text-align:center; padding: 15px 32px; color:white; background-color:red'>Delete</button></TD>");
+
+				pw.println("</TR>");
+
+			}
+			pw.println("</TABLE>");
+			
+			
+			rs.close();
+			st.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
